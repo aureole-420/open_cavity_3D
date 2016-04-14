99f80b7bcb3bdd5179052c3f2f808c6370fc09bc
yuhui <tongyuhui2009@gmail.com>
2016-04-15 00:08:31 +0800

    convergence study I

--- numerical_validation_cmt/loadParameters.m
+++ numerical_validation_cmt/loadParameters.m
@@ -1,17 +1,22 @@
 % Geometrical parameters: open cavity
-Parameters.Lx = 0.432;
-Parameters.Ly = 0.67;
-Parameters.Lz = 0.598;
+Parameters.Lx = 0.3;
+Parameters.Ly = 0.5;
+Parameters.Lz = 0.8;
+
+% Parameters.Lx = 0.432;
+% Parameters.Ly = 0.67;
+% Parameters.Lz = 0.598;
 
 % Parameters.Lx = 1;
 % Parameters.Ly = 1;
 % Parameters.Lz = 1;
+
 % % Parameters.Nx = 20; % discretized
 % % Parameters.Ny = 30; % Ny
-Parameters.Nz = 5; % Nz
+Parameters.Nz = 7; % Nz
 
 % Parameters.P = 200; % modes for the cavity
-Parameters.Q = 5; % radiation modes for the open space
+Parameters.Q = 10; % radiation modes for the open space
 
 Parameters.k = 10*2*pi/340; % frequency
 
--- numerical_validation_cmt/main.m
+++ numerical_validation_cmt/main.m
@@ -1,3 +1,4 @@
+% task 1: plot the frequency 
 
 mappingmodes2D;
 clear;close all;clc;
@@ -16,9 +17,9 @@ rho0 = Parameters.rho0 ;
 c0 = Parameters.c0 ;
 
 x0 = 0.1; y0=0.1; z0= -Lz+0.1;
-xp = 0.2; yp = 0.3; zp = -Lz +0.3;
+xp = 0.2; yp = 0.3; zp = -Lz +0.4;
 
-f = 500;
+f = 300;
 k = 2*pi*f/340;
 Parameters.k = k;
 q0 = (10^-4)*rho0*k*c0;
@@ -56,7 +57,7 @@ I = eye(P,P);
 a = (K + H*(Z\M) -(k^2)*I)\S;
 D =K + H*(Z\M);
 lambda = sort(eig(D));
-% a = (K  -(k^2)*I)\S;    
+% a = (K  -(k^2)*I)\S;
 %--------------------------pr---------------------------------------------%
 pr = 0;
 for p = 1:P
@@ -69,6 +70,62 @@ for p = 1:P
 end
 abs(pr)
 %-------------------------bi-orthorgonal modes----------------------------%
+D =K + H*(Z\M);
+[V,lambda_temp] = eig(D);
+lambda = zeros(P,1);
+for p = 1:P
+    lambda(p) = lambda_temp(p,p);
+end
+exp_mat = [lambda, V.'];
+exp_mat = sortrows(exp_mat);
+zzz = sort(eig(D));
+%decide the n^th eigen modes'
+% Nn = 15;
+% for n = 1:Nn
+%     eval(['v',num2str(n),'= exp_mat(',num2str(n),',2:P+1);']); %vn = exp_mat(n,2:P+1);
+%     eval(['[maxvalue, index] = max(v',num2str(n),');']); %[maxvalue,index] = max(vn);
+%     eval(['u',num2str(n),'= Modes3D(index,2);']); %un = Modes3D(index,2);
+%     eval(['v',num2str(n),'= Modes3D(index,2);']); %vn = Modes3D(index,3);
+%     eval(['w',num2str(n),'= Modes3D(index,2);']); %wn = Modes3D(index,4);
+% end
+num = 1
+for Nn = 1:1:40
+% Nn = 25
+    pr_recstr = 0;
+    for n = 1:Nn
+        vn = exp_mat(n,2:P+1);vn = vn.';
+        lambda_n = exp_mat(n,1);
+        c_n = (vn.'*S)/((lambda_n-k^2)*(vn.'*vn))
+        pr_n = 0;
+        for p = 1:P
+            u = Modes3D(p,2);
+            v = Modes3D(p,3);
+            w = Modes3D(p,4);
+            ku = u*pi/Lx; kv = v*pi/Ly; kw = w*pi/Lz;
+            constant = sqrt((2-KronDelta(0,u))/Lx)*sqrt((2-KronDelta(0,v))/Ly)*sqrt((2-KronDelta(0,w))/Lz);
+            pr_n = pr_n + vn(p)*constant*cos(ku*xp)*cos(kv*yp)*cos(kw*zp);
+        end
+        pr_n
+        pr_recstr = pr_recstr + c_n*pr_n;
+    end
+    NoM(num) = Nn;
+    pr_Nn(num) = abs(pr_recstr);
+    ph_Nn(num) = phase(pr_recstr);
+    num = num +1
+end
+figure(1)
+subplot(2,1,1)
+plot(NoM,pr_Nn) % ,'LineStyle','none','Marker','o'
+xlabel('Number of eigenmodes used for calculation');
+ylabel('Sound pressure (pa)')
+subplot(2,1,2)
+plot(NoM,ph_Nn)
+xlabel('Number of eigenmodes used for calculation');
+ylabel('Phase')
+abs(pr_recstr)
+
+
+
 
 
 
